dfa246d39f6f15d68bc696b8379d2fe4e1b924f0
Shady Attia <47541895+AttiaFX@users.noreply.github.com>
2019-09-02 00:09:49 +0100

    Add files via upload

---
+++ utils/memberStats.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const analytics = require("./analytics.js");
+
+const memberStats =
+    {
+        bmi: "",
+        bmiCategory: "",
+        isIdealBodyWeight: "",
+        trend: ""
+
+    };
+
+module.exports = memberStats;
