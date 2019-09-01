90ce460f1299c154ee68e89904ef238aa48abc1f
Shady Attia <47541895+AttiaFX@users.noreply.github.com>
2019-09-02 00:13:04 +0100

    Add files via upload

---
+++ models/assessment-store.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const _ = require("lodash");
+const JsonStore = require("./json-store");
+
+const assessmentStore = {
+  store: new JsonStore("./models/assessment-store.json", {
+    assessmentCollection: []
+  }),
+  collection: "assessmentCollection",
+
+  getAllAssessment() {
+    return this.store.findAll(this.collection);
+  },
+
+  getAssessment(id) {
+    return this.store.findOneBy(this.collection, { id: id });
+  },
+
+  getUserAssessments(userid) {
+    return this.store.findBy(this.collection, { userid: userid });
+  },
+
+  addAssessment(assessment) {
+    this.store.add(this.collection, assessment);
+    this.store.save();
+  },
+
+  removeAssessment(id) {
+    const assessment = this.getAssessment(id);
+    this.store.remove(this.collection, assessment);
+    this.store.save();
+  },
+
+  removeAllAssessments() {
+    this.store.removeAll(this.collection);
+    this.store.save();
+  },
+};
+
+module.exports = assessmentStore;
